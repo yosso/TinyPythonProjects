fcbc85446a55d6544d09ff0511742dc6e35c86eb
Michael S Jones <mike@mikeandmaryjones.org>
2023-03-24 16:04:36 -0500

    Add files via upload

---
+++ jump02.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Jump the Five"""
+
+import argparse
+
+# -------------------------------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='Jump the Five',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument ('text', metavar ='str', help='Input text')
+
+    return parser.parse_args()
+
+# -------------------------------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+    args = get_args()
+    jumper = {'1':'9','2': '8', '3':'7', '4':'6', '5':'0',  
+            '6': '4', '7': '3', '8': '2', '9': '1', '0': '5'}
+
+    new_text = []
+    for char in args.text:
+        new_text.append(jumper.get(char, char))
+    
+    print(''.join(new_text))
+
+# -------------------------------------------------------------------------
+if __name__ == '__main__':
+    main()
